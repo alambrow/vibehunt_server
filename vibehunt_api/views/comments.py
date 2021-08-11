38978fb57ce943ed027f55a75576bc9ba7adb5cc
alambrow <alexander.lambrow@gmail.com>
2021-08-11 15:54:44 -0500

    added ability to use query string param for comments queries

--- vibehunt_api/views/comments.py
+++ vibehunt_api/views/comments.py
@@ -6,14 +6,26 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from vibehunt_api.models import Comment, Venue
+import time
 
 class CommentView(ViewSet):
 
     def list(self, request):
-        comments = Comment.objects.all()
+        venue = self.request.query_params.get('venueId', None)
+
+        if venue:
+            comments = Comment.objects.filter(venueId=venue)
+        else:
+            comments = Comment.objects.all()
+        
+        time_window = time.time() - 7200
+        filteredComments = []
+        for comment in comments:
+            if comment.timestamp >= time_window:
+                filteredComments.append(comment)
 
         serializer = CommentSerializer(
-            comments, many=True, context={'request': request}
+            filteredComments, many=True, context={'request': request}
         )
         return Response(serializer.data)
     
