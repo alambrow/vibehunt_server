24a586baaa744117ef6a2ca5bb82c305c96d77ca
alambrow <alexander.lambrow@gmail.com>
2021-08-25 13:24:32 -0500

    changes for heroku deployment

--- vibehunt_server/settings.py
+++ vibehunt_server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1s#oeg8bkj=qz-^v=5)x!9no*5%j)*ajw$h@279*8jyv$k(a9d'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
