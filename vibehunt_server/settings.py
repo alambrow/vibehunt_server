32ab289f6554bba492374045ffbe50741a847f86
alambrow <alexander.lambrow@gmail.com>
2021-08-25 15:09:13 -0500

    edited whitelist url

--- vibehunt_server/settings.py
+++ vibehunt_server/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://vibehunt-client.herokuapp.com/'
+    'https://vibehunt-client.herokuapp.com'
 )
 
 MIDDLEWARE = [
