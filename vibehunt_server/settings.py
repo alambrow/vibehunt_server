4bc5236f772e0ed50b135014539221630edfe850
alambrow <alexander.lambrow@gmail.com>
2021-08-26 09:08:46 -0500

    changed debug settings

--- vibehunt_server/settings.py
+++ vibehunt_server/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = []
 
