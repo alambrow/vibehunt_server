52fa9872bea0a91c478c7c55bd76392af2a64a72
alambrow <alexander.lambrow@gmail.com>
2021-09-04 16:21:38 -0500

    changed settings

--- vibehunt_server/settings.py
+++ vibehunt_server/settings.py
@@ -49,6 +49,8 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000/',
 )
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
